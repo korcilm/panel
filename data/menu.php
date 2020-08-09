7c737c180ca264a10829ca60e5b727e1d2bc0c7c
korcilm <58364191+korcilm@users.noreply.github.com>
2020-08-09 15:40:56 +0300

    Update menu.php

--- data/menu.php
+++ data/menu.php
@@ -1,5 +1,4 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-
   <!-- Sidebar -->
   <div class="sidebar">
     <!-- Sidebar user panel (optional) -->
@@ -11,22 +10,9 @@
         <a href="<?=SITE?>" class="d-block"><?=$_SESSION["adsoyad"]?></a>
       </div>
     </div>
-
-  
     <!-- Sidebar Menu -->
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-           with font-awesome or any other icon font library -->
-           <li class="nav-item">
-            <a href="<?=SITE?>modul-ekle" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Modül
-              </p>
-            </a>
-          </li>
-
           <li class="nav-item has-treeview menu-open">
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
